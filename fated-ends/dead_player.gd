cff8e6aa740fc5e5a13fc915b3bdaf18567a4c2a
SampaiWasTaken <samuel.radler@htl-klu.at>
2024-12-13 23:43:57 +0100

    Ok

--- fated-ends/dead_player.gd
+++ fated-ends/dead_player.gd
@@ -1,12 +1,11 @@
-extends Skeleton3D
+extends Node3D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	physical_bones_start_simulation()
+func _ready() -> void:
+	$AnimationPlayer.play("mixamo_com")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	physical_bones_start_simulation()
 	pass
