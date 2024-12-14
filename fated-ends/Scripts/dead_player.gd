6d75a22200535bad8d99ecdc74210986ed47ebce
SampaiWasTaken <samuel.radler@htl-klu.at>
2024-12-14 22:33:00 +0100

    intro scene added

--- fated-ends/Scripts/dead_player.gd
+++ fated-ends/Scripts/dead_player.gd
@@ -10,8 +10,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-	
-
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	$RigidBody3D/CollisionShape3D.disabled = false
 	$RigidBody3D.freeze = false
